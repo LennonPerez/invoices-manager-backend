412fb50220b26e9ed2e85552cee1540785c81193
Lennon <lenonart14@gmail.com>
2023-04-16 12:29:47 -0400

    fix

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,19 +1,22 @@
+import path from "path";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import Invoice from "../entities/Invoice";
-import InvoiceItem from "../entities/Item";
+// import Invoice from "../entities/Invoice";
+// import InvoiceItem from "../entities/Item";
+
+const isCompiled = path.extname(__filename).includes("js");
 
 const appDataSource: DataSource = new DataSource({
   type: "postgres",
-  port: Number(process.env.DB_PORT),
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  // database: "invoices",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  host: process.env.DB_HOST || "localhost",
+  username: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "invoices",
   logging: true,
   synchronize: true,
-  entities: [Invoice, InvoiceItem],
+  entities: [`src/entities/**/*.${isCompiled ? "js" : "ts"}`],
+  // entities: [Invoice, InvoiceItem],
 });
 
 export default appDataSource;
